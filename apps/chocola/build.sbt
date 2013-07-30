5398e69fb5597109ec5ef5ad40aa283753334cac
numa08 <n511287@gmail.com>
2013-07-30 22:53:15 +0900

    Akka使う

--- apps/chocola/build.sbt
+++ apps/chocola/build.sbt
@@ -5,3 +5,7 @@ libraryDependencies += "commons-lang" % "commons-lang" % "2.6"
 libraryDependencies += "commons-daemon" % "commons-daemon" % "1.0.10"
 
 libraryDependencies += "com.twitter" % "hbc-twitter4j-v3" % "1.4.0"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+ 
+libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0.5"
--- apps/chocola/src/main/scala/net/numa08/models/DirectMessage.scala
+++ apps/chocola/src/main/scala/net/numa08/models/DirectMessage.scala
@@ -1,11 +1,15 @@
 package net.numa08.models
 
+import akka.actor.Actor
+import akka.actor.Props
+import akka.event.Logging
+
 import twitter4j.Twitter
 import twitter4j.TwitterException
 import twitter4j.TwitterFactory
 import twitter4j.conf.Configuration
 
-case class DirectMessage(val message:String, val config:Configuration) extends  Notification{
+case class DirectMessage(val message:String, val config:Configuration) extends  Notification with Actor{
 		def notifTo(destination:NotifDestination){
 			val text = message.substring(0, 140)
 
@@ -14,6 +18,12 @@ case class DirectMessage(val message:String, val config:Configuration) extends  
 			import twitter4j.DirectMessage
 			val twitter = new TwitterFactory(config).getInstance()
 			twitter.sendDirectMessage(destination.identifier, text)
+		}
+
+		val log = Logging(context.system, this)
+		def receive = {
+			case "test" ⇒ log.info("received test")
+			case _      ⇒ log.info("received unknown message")
 		}
 }
 
