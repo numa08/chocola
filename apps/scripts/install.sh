415e64054070fcb9ea985783d63635b382256cfb
numa08 <n511287@gmail.com>
2013-08-06 21:56:48 +0900

    スクリプトの改行コード修正
    スクリプトのファイル名修正

--- apps/scripts/install.sh
+++ apps/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 current=$(cd $(dirname $0);pwd)
 
-cp -f ${current}/usr/local/etc/chocola.sh /usr/local/etc/chocola
+cp -f ${current}/usr/local/etc/chocola /usr/local/etc/chocola
 chmod 755 /usr/local/etc/chocola
 
-cp -f ${current}/etc/rc.d/init.d/chocola.sh /etc/rc.d/init.d/chocola
+cp -f ${current}/etc/rc.d/init.d/chocola /etc/rc.d/init.d/chocola
 chmod 755 /etc/rc.d/init.d/chocola
 
 cp -f ${current}/../chocola/target/scala-2.9.3/chocola.jar /usr/local/bin/chocola.jar
--- apps/scripts/usr/local/etc/chocola.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#config file for chocola
-CHOCOLA=/usr/local/bin/chocola.jar
-CHOCOLA_PID=/usr/local/etc/chocola.pid
-CHOCOLA_MAIN=net.numa08.chocola.Chocola
