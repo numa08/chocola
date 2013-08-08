9ff55d11850eb13788c27f8e15a5dae7bc1ef834
numa08 <n511287@gmail.com>
2013-08-08 23:38:01 +0900

    フィルターを修正

--- apps/chocola/src/main/scala/net/numa08/models/NumaFilter.scala
+++ apps/chocola/src/main/scala/net/numa08/models/NumaFilter.scala
@@ -8,13 +8,13 @@ class NumaFilter(status: Status) extends Filter[Status]{
 			val filterRule = (status:Status) => {
 				val pattern = ".*([nNｎＮ][uUｕＵ][mMｍＭ][aAａＡ]|ぬま|沼|[ヌﾇ][マﾏ])(さん|08|3|4|３|４)?.*"
 				val matches = status.getText.matches(pattern)// && status.getInReplyToScreenName.isEmpty
-        val isNotReply = Option(status.getInReplyToScreenName) match {
-          case Some(name) => name.isEmpty
-          case None => false
-        }
+//        val isNotReply = Option(status.getInReplyToScreenName) match {
+//          case Some(name) => name.isEmpty
+//          case None => false
+//        }
         val isNotRetweet = !status.isRetweet
 
-				matches && isNotReply && isNotRetweet
+				matches && /*isNotReply &&*/ isNotRetweet
 			}
 
       Option(status).filter(filterRule).foreach(c)
