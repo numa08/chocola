ccb769721f74abb9fdee177a0b2ba71cedb4a7b2
numa08 <n511287@gmail.com>
2013-08-08 22:00:29 +0900

    フィルターうぃ作ったよ

---
+++ apps/chocola/src/main/scala/net/numa08/models/NumaFilter.scala
@@ -0,0 +1,22 @@
+package net.numa08.models
+
+import twitter4j.Status
+
+class NumaFilter(status: Status) extends Filter[Status]{
+
+  def filter(c:Status => Unit){
+			val filterRule = (status:Status) => {
+				val pattern = ".*([nNｎＮ][uUｕＵ][mMｍＭ][aAａＡ]|ぬま|沼|[ヌﾇ][マﾏ])(さん|08|3|4|３|４)?.*"
+				val matches = status.getText.matches(pattern)// && status.getInReplyToScreenName.isEmpty
+        val isNotReply = Option(status.getInReplyToScreenName) match {
+          case Some(name) => name.isEmpty
+          case None => false
+        }
+        val isNotRetweet = !status.isRetweet
+
+				matches && isNotReply && isNotRetweet
+			}
+
+      Option(status).filter(filterRule).foreach(c)
+  }
+}
