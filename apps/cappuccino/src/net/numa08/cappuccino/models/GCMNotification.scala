abb43ba48644f2a8c48eb87c9b3868fef6280658
numa08 <n511287@gmail.com>
2013-08-31 01:35:13 +0000

    GCMの送信を実装

--- apps/cappuccino/src/net/numa08/cappuccino/models/GCMNotification.scala
+++ apps/cappuccino/src/net/numa08/cappuccino/models/GCMNotification.scala
@@ -1,6 +1,32 @@
 package net.numa08.cappuccino.models
 
-class GCMNotification extends Notification {
+import com.google.android.gcm.server.Message
+import com.google.android.gcm.server.Sender
+import scala.actors.Actor
 
-  def notifTo (destination : NotifDestination) {}
+class GCMNotification(message : String) extends Notification {
+
+  def notifTo (destination : NotifDestination) {
+    val gcmMessage = new Message.Builder().addData(GCMNotification.MESSAGE, message)
+                                           .build
+    
+    val messenger = system.actorOf(Props(AsyncMessenger(gcmMessage, destination)), name = "messenger")
+    messenger ! GCMNotification.GCMSender
+  }
+}
+
+object GCMNotification {
+  private val MESSAGE = "net.numa08.chocola.text"
+  private val SENDER_ID = ""
+
+  private lazy val GCMSender = new Sender(GCMNotification.SENDER_ID)
+}
+
+case class AsynMessenger(message : Message, destination : NotifDestination) extends Actor {
+
+  def receive = {
+    case sender : Sender => {
+      sender.msendNoRetry(message, destination.identifier)
+    }
+  }
 }
